f67b4598e206e5474e7ea03ad67edd8f43f26dcb
Danieliao <1246216138@qq.com>
2020-03-21 11:05:33 -0700

    Update bp_fe_bht.v

--- bp_fe/src/v/bp_fe_bht.v
+++ bp_fe/src/v/bp_fe_bht.v
@@ -12,7 +12,10 @@ module bp_fe_bht
  import bp_fe_pkg::*; 
  #(parameter bht_idx_width_p = "inv"
 
-   , localparam els_lp             = 2**bht_idx_width_p
+   , localparam local_bht_els_lp   = 2**bht_idx_width_p
+   , localparam local_bht_length   = 4 //set to even number
+   , localparam global_bht_idx_width_p = 9
+   , localparam global_bht_els_lp  = 2**global_bht_idx_width_p
    , localparam saturation_size_lp = 2
    )
   (input                         clk_i
@@ -27,28 +30,52 @@ module bp_fe_bht
    , output                      predict_o
    );
 
-logic [els_lp-1:0][saturation_size_lp-1:0] mem;
 
-assign predict_o = r_v_i ? mem[idx_r_i][1] : 1'b0;
+
+//logic [els_lp-1:0][saturation_size_lp-1:0] global_bht;
+logic [local_bht_els_lp-1:0][local_bht_length-1:0] local_bht;
+logic [global_bht_els_lp-1:0][saturation_size_lp-1:0] global_bht;
+
+logic [global_bht_idx_width_p-1:0] global_bht_index;
+assign global_bht_index = {idx_w_i[global_bht_idx_width_p-local_bht_length-1:0], local_bht[idx_w_i]};
+
+logic [saturation_size_lp-1:0] global_bht_r;
+
+
+always_ff @(posedge clk_i)
+  begin 
+    if (reset_i)
+      local_bht <= '{default:4'b0000};
+      //global_bht_r <= 2'b0;
+    else if (w_v_i)
+      local_bht[idx_w_i] <= {global_bht_r, local_bht[idx_w_i][local_bht_length-1:local_bht_length-1-saturation_size_lp]};
+end
+
+assign predict_o = r_v_i ? global_bht_r[1] : 1'b0;
 
 always_ff @(posedge clk_i) 
   if (reset_i) 
-    mem <= '{default:2'b01};
+    begin
+      global_bht <= '{default:2'b01};
+      global_bht_r <= 2'b01;
+    end
   else if (w_v_i) 
     begin
       //2-bit saturating counter(high_bit:prediction direction,low_bit:strong/weak prediction)
-      case ({correct_i, mem[idx_w_i][1], mem[idx_w_i][0]})
+      case ({correct_i, global_bht[global_bht_index][1], global_bht[global_bht_index][0]})
         //wrong prediction
-        3'b000: mem[idx_w_i] <= {mem[idx_w_i][1]^mem[idx_w_i][0], 1'b1};//2'b01
-        3'b001: mem[idx_w_i] <= {mem[idx_w_i][1]^mem[idx_w_i][0], 1'b1};//2'b11
-        3'b010: mem[idx_w_i] <= {mem[idx_w_i][1]^mem[idx_w_i][0], 1'b1};//2'b11
-        3'b011: mem[idx_w_i] <= {mem[idx_w_i][1]^mem[idx_w_i][0], 1'b1};//2'b01
+        3'b000: global_bht_r <= {global_bht[global_bht_index][1]^global_bht[global_bht_index][0], 1'b1};//2'b01
+        3'b001: global_bht_r <= {global_bht[global_bht_index][1]^global_bht[global_bht_index][0], 1'b1};//2'b11
+        3'b010: global_bht_r <= {global_bht[global_bht_index][1]^global_bht[global_bht_index][0], 1'b1};//2'b11
+        3'b011: global_bht_r <= {global_bht[global_bht_index][1]^global_bht[global_bht_index][0], 1'b1};//2'b01
         //correct prediction
-        3'b100: mem[idx_w_i] <= mem[idx_w_i];//2'b00
-        3'b101: mem[idx_w_i] <= {mem[idx_w_i][1], ~mem[idx_w_i][0]};//2'b00
-        3'b110: mem[idx_w_i] <= mem[idx_w_i];//2'b10
-        3'b111: mem[idx_w_i] <= {mem[idx_w_i][1], ~mem[idx_w_i][0]};//2'b10
+        3'b100: global_bht_r <= global_bht[global_bht_index];//2'b00
+        3'b101: global_bht_r <= {global_bht[global_bht_index][1], ~global_bht[global_bht_index][0]};//2'b00
+        3'b110: global_bht_r <= global_bht[global_bht_index];//2'b10
+        3'b111: global_bht_r <= {global_bht[global_bht_index][1], ~global_bht[global_bht_index][0]};//2'b10
       endcase
-    end
 
+    global_bht[global_bht_index] <= global_bht_r;
+    end
+  
 endmodule
